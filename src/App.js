72ca5e1e27c8f4af887d6f2b2e9e6f350a842639
ferLaika <fedesalvideal@outlook.com>
2024-09-02 12:46:02 -0500

    server works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import ExerciseLibrary from './pages/ExerciseLibrary';
 import WorkoutBuilder from './pages/WorkoutBuilder';
-
-
+import './styles/App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/exercises" component={ExerciseLibrary} />
-          <Route path="/workout-builder" component={WorkoutBuilder} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercises" element={<ExerciseLibrary />} />
+          <Route path="/workout-builder" element={<WorkoutBuilder />} />
+        </Routes>
       </div>
     </Router>
   );
 }
-
 
 export default App;
 
